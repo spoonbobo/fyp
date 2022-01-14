f63682c88034270510ce248094aaee0362940ee1
spoonnvidia <u3575272@connect.hku.hk>
2022-01-14 00:28:46 +0000

    first commit

---
+++ cfg.py
@@ -0,0 +1,7 @@
+PLOT_THEME = "seaborn-colorblind"
+
+PLOT_FONT = {'family' : 'sans-serif',
+             'weight' : 'bold',
+             'size'   : 20}
+
+HONG_KONG_BASEMAP_CFG = {"resolution": 'f', "projection":'merc', "lat_0": 22.394400, "lon_0": 114.156489, "llcrnrlon": 113.80779, "llcrnrlat": 22.16694, "urcrnrlon": 114.416158, "urcrnrlat": 22.5783}
